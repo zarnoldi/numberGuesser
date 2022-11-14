157744c96f38923b6d52efdd5dbd27be1e9ca7d1
Tom Arnold <tarnold@kfh.co.uk>
2022-11-14 18:12:38 +0000

    data gathering and input validation completed

--- app.js
+++ app.js
@@ -13,8 +13,52 @@ let min = 1,
     guessesLeft = 3;
 
 // UI Elements
-const game = document.getElementById('game'), 
+const game = document.querySelector('#game'), 
       minNum = document.querySelector('.minNum'),
       maxNum = document.querySelector('.maxNum'), 
-      guessBtn = document.getElementById('guessValue'); 
+      guessBtn = document.querySelector('#guessBtn'), 
+      message = document.querySelector('#message'), 
+      guessInput = document.querySelector('#guessInput');
 
+
+// Assign min, max to UI values 
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Create Event listner for BTN
+
+game.addEventListener('submit', getInputValue);
+
+function getInputValue(e) {
+
+  let guess = parseInt(guessInput.value); 
+
+  setMessage(null, null)
+
+   //   Validation
+   if (guess === NaN || guess < min || guess > max) {
+    setMessage(`Your guess is outside the games Min and Max range of
+                    ${min} and ${max}, please try again`, 
+                    'alert-danger')
+   }else{
+    console.log('guess fine');
+   }
+
+  console.log(guess);
+
+  e.preventDefault(); 
+}
+
+function setMessage(userMessage, style) {
+
+    if (userMessage === null && style === null ) {
+        message.textContent = null; 
+        message.className = 'alert'; 
+        
+    } else {
+        message.textContent = userMessage; 
+        message.classList.add(style);
+
+    }
+
+}
