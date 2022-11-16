d18da8aa7f535ca14ef4121a1214ae0dcc2fd6b0
Tom Arnold <tarnold@kfh.co.uk>
2022-11-16 17:09:47 +0000

    Completed - added play again functionality and all other game play complete

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Games values
 let min = 1, 
     max = 10,
-    winningNum,
+    winningNum = Math.floor(Math.random()*max),
     guessesLeft = 3;
 
 // UI Elements
@@ -25,22 +25,26 @@ const game = document.querySelector('#game'),
 minNum.textContent = min;
 maxNum.textContent = max;
 
-// Create Event listner for BTN
-
+// Create Event listner for Guess BTN
 game.addEventListener('submit', getInputValue);
+
+// Create Event Listener for Play Again BTN
+game.addEventListener('mousedown', (e)=>{
+    console.log(e.target);
+
+    if (e.target.classList.contains('playAgain')){
+        window.location.reload();
+    }
+})
 
 function getInputValue(e) {
 
-   const winningNum = Math.floor(Math.random()*max);
-
-   console.log(winningNum);
-  
     let guess = parseInt(guessInput.value); 
 
    //Reset message 
   setMessage(null, null)
 
-   //   Validation
+   // Validation
    if (isNaN(guess) || guess < min || guess > max) {
     setMessage(`Your guess is outside the games Min and Max range of
                     ${min} and ${max}, please try again`, 
@@ -48,15 +52,16 @@ function getInputValue(e) {
    }
    else if(guess === winningNum){
     setMessage(`Your guess of ${guess} was correct!!`, 
-                    'alert-success')
+                    'alert-success');
+    playAgain(); 
    }else if(guessesLeft) {
     setMessage(`Your guess of ${guess} was incorrect, you have ${guessesLeft} guesses left`, 
                     'alert-warning')
     guessesLeft = guessesLeft - 1; 
-    console.log(guessesLeft);
    }else{
     setMessage(`You've run out of guesses :(`, 
-                    'alert-secondary')
+                    'alert-secondary');
+    playAgain(); 
    }
 
   e.preventDefault(); 
@@ -67,11 +72,16 @@ function setMessage(userMessage, style) {
     if (userMessage === null && style === null ) {
         message.textContent = null; 
         message.className = 'alert'; 
-        
     } else {
         message.textContent = userMessage; 
         message.classList.add(style);
-
     }
+}
 
+function playAgain() {
+    
+    guessBtn.textContent = 'Play Again';
+    guessBtn.className += ' playAgain'; 
+
+    
 }
